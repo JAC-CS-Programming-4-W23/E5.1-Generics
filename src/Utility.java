fee41f20b3decef2a318639d819fadb9a76342e1
Vikram Singh <vikram.singh@johnabbott.qc.ca>
2023-02-19 15:39:39 -0500

    Added solution.

--- src/Utility.java
+++ src/Utility.java
@@ -12,7 +12,9 @@ public class Utility {
      * @param j The second index.
      */
     public static <T> void swap(T[] array, int i, int j) {
-
+        T holder = array[j];
+        array[j] = array[i];
+        array[i] = holder;
     }
 
     /**
@@ -22,7 +24,26 @@ public class Utility {
      * @return The number of occurrences of the element inside the array.
      */
     public static <T> int count(T[] array, T element) {
+        int count = 0;
 
+        // can allow counting of null references
+        // why though?
+        if (element == null) {
+            for (T listItem : array) {
+                if (listItem == null) {
+                    count++;
+                }
+            }
+        }
+        else {
+            for (T listItem : array) {
+                if (element.equals(listItem)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
     }
 
     /**
@@ -32,7 +53,13 @@ public class Utility {
      * @return The index of the element in the array. -1 if not found.
      */
     public static <T> int find(T[] array, T element) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(element)) {
+                return i;
+            }
+        }
 
+        return -1;
     }
 
     /**
@@ -43,27 +70,92 @@ public class Utility {
      * @return The size of the array minus any removals.
      */
     public static <T> int filter(T[] array, int size, T element) {
+        for (int i = 0; i < size; i++) {
+            if (array[i].equals(element)) {
+                for (int j = i; j < size - 1; j++) {
+                    array[j] = array[j + 1];
+                }
 
+                array[size - 1] = null; // TODO: explain when we cover references
+                size--;
+                i--;
+            }
+        }
+
+        return size;
     }
 
     public static <T> int filterWith(T[] array, int size, Predicate<T> p) {
+        for (int i = 0; i < size; i++) {
+            if (p.test(array[i])) {
+                for (int j = i; j < size - 1; j++) {
+                    array[j] = array[j + 1];
+                }
 
+                array[size - 1] = null; // TODO: explain when we cover references
+                size--;
+                i--;
+            }
+        }
+
+        return size;
     }
 
     private static <T> Pair<T>[] zipPair(T[] array1, T[] array2) {
+        // the input arrays must be the same length
+        if (array1.length != array2.length) {
+            throw new IllegalArgumentException("The two input arrays must have the same length");
+        }
 
+        // "zip" together the two input arrays into the resulting array.
+        Pair<T>[] zippedArray = (Pair<T>[]) new Pair[array1.length];
+
+        for (int i = 0; i < array1.length; i++) {
+            zippedArray[i] = new Pair<>(array1[i], array2[i]);
+        }
+
+        return zippedArray;
     }
 
     private static <S,T> Tuple<S,T>[] zipTuple(S[] array1, T[] array2) {
+        // the input arrays must be the same length
+        if (array1.length != array2.length)
+            throw new IllegalArgumentException("The two input arrays must have the same length");
 
+        // "zip" together the two input arrays into the resulting array.
+        Tuple<S, T>[] zippedArray = (Tuple<S,T>[]) new Tuple[array1.length];
+
+        for (int i = 0; i < array1.length; i++) {
+            zippedArray[i] = new Tuple<>(array1[i], array2[i]);
+        }
+
+        return zippedArray;
     }
 
     public static <R, S, T> R[] zipWith(S[] array1, T[] array2, Function<Tuple<S, T>, R> f) {
+        // the input arrays must be the same length
+        if(array1.length != array2.length) {
+            throw new IllegalArgumentException("The two input arrays must have the same length");
+        }
 
+        // "zip" together the two input arrays into the resulting array.
+        R[] zippedArray = (R[]) new Object[array1.length];
+
+        for (int i = 0; i < array1.length; i++) {
+            zippedArray[i] = f.apply(new Tuple<>(array1[i], array2[i]));
+        }
+
+        return zippedArray;
     }
 
     public static <S, T> S[] map(T[] array, Function<T, S> f) {
-        
+        S[] result = (S[]) new Object[array.length];
+
+        for(int i = 0; i < array.length; i++) {
+            result[i] = f.apply(array[i]);
+        }
+
+        return result;
     }
 
 }
